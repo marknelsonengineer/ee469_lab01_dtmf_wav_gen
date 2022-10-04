07088ffc72c7411767cee5fe491c6eece7bd0efc
Mark Nelson <marknels@hawaii.edu>
2022-10-04 11:39:38 -0700

    Add header to main.c Update comments (mostly formatting)

--- main.c
+++ main.c
@@ -1,24 +1,41 @@
-#include <stdio.h>
+///////////////////////////////////////////////////////////////////////////////
+//          University of Hawaii, College of Engineering
+//          ee469_lab01_dtmf_wav_gen - EE 469 - Fall 2022
+//
+/// Generate a .wav file containing DTMF dialing tones
+///
+/// @see https://docs.fileformat.com/audio/wav/
+///
+/// @file main.c
+/// @version 1.0
+///
+/// @author Mark Nelson <marknels@hawaii.edu>
+/// @date   04_Oct_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>   // For printf(), fopen(), etc.
 #include <stdlib.h>  // For EXIT_SUCCESS
-#include <assert.h>
+#include <assert.h>  // For assert()
 #include <stdint.h>  // For fixed-length ints
 #include <math.h>    // For sin()
 
-#define PROGRAM_NAME "ee469_dtmf_wav_gen"
-#define FILENAME "/home/mark/src/tmp/blob.wav"
+#define PROGRAM_NAME "ee469_lab01_dtmf_wav_gen"
+#define FILENAME     "/home/mark/src/tmp/blob.wav"
 
-#define CHANNELS 1
-#define SAMPLE_RATE 8000       /* Samples per second */
-#define BYTES_PER_SAMPLE 1     /* (BitsPerSample * Channels) / 8     1 - 8 bit mono     2 - 8 bit stereo/16 bit mono4 - 16 bit stereo */
-#define BITS_PER_SAMPLE 8
-#define DURATION_IN_SECONDS 2
-#define AMPLITUDE 0.1          /* Max ampltude of signal */
+#define CHANNELS            1     /* 1 for mono   2 for stereo         */
+#define SAMPLE_RATE      8000     /* Samples per second                */
+#define BYTES_PER_SAMPLE    1     /* (BitsPerSample * Channels) / 8
+                                     1 - 8 bit mono
+                                     2 - 8 bit stereo/16 bit mono
+                                     4 - 16 bit stereo                 */
+#define BITS_PER_SAMPLE     8     /* 256 possible values               */
+#define DURATION_IN_SECONDS 2     /* Each DTMF tone is 2 seconds long  */
+#define AMPLITUDE           0.1   /* Max amplitude of signal           */
 
-#define TONE 500
+#define TONE                500
 
 FILE *fp = NULL;
 
-/// @see https://docs.fileformat.com/audio/wav/
 void write_header() {
    assert( fp != NULL );
 
@@ -27,17 +44,18 @@ void write_header() {
    /// Marks file as a RIFF file
    fwrite( "RIFF", 1, 4, fp );  /// @todo Add error checking
 
-   uint32_t file_size = 44 + pcm_data_size;  // 44 is the actual size of the header (regardless of what you see below)
+   uint32_t file_size = 44 + pcm_data_size;  // 44 is the actual size of the header
+                                             // (regardless of what you see below)
    fwrite( &file_size, 1, 4, fp );  /// File size
 
-   fwrite( "WAVE", 1, 4, fp );  /// File type header
+   fwrite( "WAVE", 1, 4, fp );      /// File type header
 
-   fprintf( fp, "fmt ", 0 );  /// Format chunk marker (includes trailing space)
+   fwrite( "fmt ", 1, 4, fp );      /// Format chunk marker (includes trailing space)
 
-   uint32_t header_length = 16;  /// The length of the header (above)
+   uint32_t header_length = 16;     /// The length of the header (above)
    fwrite( &header_length, 1, 4, fp );
 
-   uint16_t format_code = 1;   /// 1==PCM
+   uint16_t format_code = 1;        /// 1==PCM
    fwrite( &format_code, 1, 2, fp );
 
    uint16_t channels = CHANNELS;
