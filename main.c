9a12cd8d1b13a51684da0c126afb7d93e42199f3
Mark Nelson <marknels@hawaii.edu>
2022-10-04 12:03:15 -0700

    Refactor the program to write arbitrarily long .wav files.

--- main.c
+++ main.c
@@ -34,52 +34,65 @@
 
 #define TONE                500
 
-FILE *fp = NULL;
+FILE *gFile = NULL;               /* Global file pointer to FILENAME */
 
-void write_header() {
-   assert( fp != NULL );
+uint32_t gPCM_data_size = 0;      /* Global counter of the bytes written */
 
-   uint32_t pcm_data_size = DURATION_IN_SECONDS * SAMPLE_RATE * BYTES_PER_SAMPLE;
+void open_audio_file() {
+   assert( gFile == NULL );
+
+   gFile = fopen( FILENAME, "w+" );
+   if( gFile == NULL ) {
+      printf(PROGRAM_NAME ": Could not open file [%s].  Exiting.\n", FILENAME );
+      exit( EXIT_FAILURE );
+   }
+
+   assert( gFile != NULL );
+
+   gPCM_data_size = 0;
 
    /// Marks file as a RIFF file
-   fwrite( "RIFF", 1, 4, fp );  /// @todo Add error checking
+   fwrite( "RIFF", 1, 4, gFile );  /// @todo Add error checking
 
-   uint32_t file_size = 44 + pcm_data_size;  // 44 is the actual size of the header
-                                             // (regardless of what you see below)
-   fwrite( &file_size, 1, 4, fp );  /// File size
+   uint32_t file_size = 44 + gPCM_data_size;  // 44 is the actual size of the header
+                                              // (regardless of what you see below)
+                                              // This gets overwritten later
+   fwrite( &file_size, 1, 4, gFile );  /// File size
 
-   fwrite( "WAVE", 1, 4, fp );      /// File type header
+   fwrite( "WAVE", 1, 4, gFile );      /// File type header
 
-   fwrite( "fmt ", 1, 4, fp );      /// Format chunk marker (includes trailing space)
+   fwrite( "fmt ", 1, 4, gFile );      /// Format chunk marker (includes trailing space)
 
    uint32_t header_length = 16;     /// The length of the header (above)
-   fwrite( &header_length, 1, 4, fp );
+   fwrite( &header_length, 1, 4, gFile );
 
    uint16_t format_code = 1;        /// 1==PCM
-   fwrite( &format_code, 1, 2, fp );
+   fwrite( &format_code, 1, 2, gFile );
 
    uint16_t channels = CHANNELS;
-   fwrite( &channels, 1, 2, fp );
+   fwrite( &channels, 1, 2, gFile );
 
    uint32_t sample_rate = SAMPLE_RATE;
-   fwrite( &sample_rate, 1, 4, fp );
+   fwrite( &sample_rate, 1, 4, gFile );
 
    uint32_t stream_rate = ( SAMPLE_RATE * BITS_PER_SAMPLE * CHANNELS) / 8;  /// (Sample Rate * BitsPerSample * Channels) / 8
-   fwrite( &stream_rate, 1, 4, fp );
+   fwrite( &stream_rate, 1, 4, gFile );
 
    uint16_t bytes_per_sample = BYTES_PER_SAMPLE;
-   fwrite( &bytes_per_sample, 1, 2, fp );
+   fwrite( &bytes_per_sample, 1, 2, gFile );
 
    uint16_t bits_per_sample = BITS_PER_SAMPLE;
-   fwrite( &bits_per_sample, 1, 2, fp );
+   fwrite( &bits_per_sample, 1, 2, gFile );
 
-   fwrite( "data", 1, 4, fp );   /// Marks the beginning of the data section.
+   fwrite( "data", 1, 4, gFile );   /// Marks the beginning of the data section.
 
-   fwrite( &pcm_data_size, 1, 4, fp );
+   fwrite( &gPCM_data_size, 1, 4, gFile );
 }
 
 
 void write_audio() {
+   assert( gFile != NULL );
+
    uint32_t index = 0;
    uint32_t samples = DURATION_IN_SECONDS * SAMPLE_RATE;
 
@@ -92,8 +105,34 @@ void write_audio() {
       s = sin( index / cadence );
       sample = (uint8_t) 128 + ( s * 256 * AMPLITUDE );
 
-      fwrite( &sample, 1, 1, fp );
+      size_t bytes_written = fwrite( &sample, 1, 1, gFile );
+      if( bytes_written != 1 ) {
+         printf(PROGRAM_NAME ": Unable to stream PCM to [%s].  Exiting.\n", FILENAME );
+         exit( EXIT_FAILURE );
+      }
+
+      gPCM_data_size++;
       index++;
+   }
+}
+
+
+void close_audio_file() {
+   assert( gFile != NULL );
+
+   fseek( gFile, 4, SEEK_SET );  /// Seek to the File Size field
+
+   uint32_t file_size = 44 + gPCM_data_size;  // 44 is the actual size of the header
+
+   fwrite( &file_size, 1, 4, gFile );         /// File size
+
+   fseek( gFile, 42, SEEK_SET );  /// Seek to the Size of the Data Section field
+
+   fwrite( &gPCM_data_size, 1, 4, gFile );
+
+   if( gFile != NULL ) {
+      fclose( gFile );
+      gFile = NULL;
    }
 }
 
@@ -101,20 +140,11 @@ void write_audio() {
 int main() {
    printf( PROGRAM_NAME ": Starting.  Writing to [%s]\n", FILENAME );
 
-   fp = fopen( FILENAME, "w+" );
-   if( fp == NULL ) {
-      printf(PROGRAM_NAME ": Could not open file [%s].  Exiting.\n", FILENAME );
-      exit( EXIT_FAILURE );
-   }
-
-   write_header();
+   open_audio_file();
 
    write_audio();
 
-   if( fp != NULL ) {
-      fclose( fp );
-      fp = NULL;
-   }
+   close_audio_file();
 
    printf( PROGRAM_NAME ": Ends successfully\n" );
    return EXIT_SUCCESS;
